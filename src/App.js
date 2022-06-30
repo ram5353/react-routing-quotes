7ef120a6c8698b39b6205c4c6a772edde201a10f
ram5353 <ramkiran5353@gmail.com>
2022-06-30 02:56:11 -0400

    added not found page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
+import NotFound from "./pages/NotFound";
 import QuoteDetails from "./pages/QuoteDetails";
 
 function App() {
@@ -20,6 +21,9 @@ function App() {
           </Route>
           <Route path="/new-quote">
             <NewQuote />
+          </Route>
+          <Route path="*">
+            <NotFound />
           </Route>
         </Switch>
       </Layout>
