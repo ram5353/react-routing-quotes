17e75ce18ed36ca9453e763c3aee389cec71173d
ram5353 <ramkiran5353@gmail.com>
2022-06-30 03:02:10 -0400

    implemented programmatic navigation

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,13 @@
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+
+  const history = useHistory();
+
   const addQuoteHandler = (quotedata) => {
     console.log(quotedata);
+    history.push("/quotes")
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
